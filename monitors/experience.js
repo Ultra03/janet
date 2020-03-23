c1d242329f69e7b5497bf8b2e1e45b1dad201e7b
1Conan <me@1conan.com>
2020-03-23 08:47:58 +0800

    add roles on specific levels

--- monitors/experience.js
+++ monitors/experience.js
@@ -14,12 +14,25 @@ module.exports = class extends Monitor {
   }
 
   async run(message) {
+    if (!message.guild) return;
     if (message.author.settings.xpFrozen) return;
     let gainedXP = Math.floor(Math.random() * 10 + 1)
     //if (message.member.lastMessage.content === message.content) gainedXP = gainedXP * -1;
     const currentXP = message.author.settings.xp;
     await message.author.settings.update('xp', currentXP + gainedXP);
-    await message.author.settings.update('level', this.getLevel(currentXP + gainedXP))
+    await message.author.settings.update('level', this.getLevel(currentXP + gainedXP));
+
+    if (message.author.settings.level >= 15) {
+      await message.member.roles.add(message.client.settings.roles.memberplus);
+    }
+
+    if (message.author.settings.level >= 30) {
+      await message.member.roles.add(message.client.settings.roles.memberpro);
+    }
+
+    if (message.author.settings.level >= 50) {
+      await message.member.roles.add(message.client.settings.roles.memberedition);
+    }
   }
 
   async init() {}
